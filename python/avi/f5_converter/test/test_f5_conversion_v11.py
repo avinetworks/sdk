8cf8bcd6cc5db8a5c293b8fc6c06fba09b39e716
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2016-06-16 13:42:06 +0530

    Updated relative path

--- python/avi/f5_converter/test/test_f5_conversion_v11.py
+++ python/avi/f5_converter/test/test_f5_conversion_v11.py
@@ -6,6 +6,7 @@ import csv
 
 import avi.f5_converter.f5_config_converter as f5_config_converter
 import avi.f5_converter.f5_parser as f5_parser
+import avi.f5_converter.f5_converter as f5_converter
 
 gSAMPLE_CONFIG = None
 log = logging.getLogger(__name__)
@@ -35,7 +36,15 @@ class Test(unittest.TestCase):
     LOG.addHandler(fh)
 
     def test_config_conversion(self):
+        dir_path = os.path.abspath(os.path.dirname(__file__))
+        dir_path = dir_path.rsplit(os.path.sep, 1)[0]
         f5_config_dict = f5_parser.parse_config(gSAMPLE_CONFIG, 11)
+
+        defaults_file = open(dir_path+"/f5_v11_defaults.conf", "r")
+        f5_defaults_dict = f5_parser.parse_config(defaults_file.read(), 11)
+        f5_converter.dict_merge(f5_defaults_dict, f5_config_dict)
+        f5_config_dict = f5_defaults_dict
+
         assert f5_config_dict.get("virtual", None)
         assert f5_config_dict.get("monitor", None)
         assert f5_config_dict.get("pool", None)
@@ -43,10 +52,9 @@ class Test(unittest.TestCase):
         assert f5_config_dict.get("node", None)
         f5_config_test = copy.deepcopy(f5_config_dict)
         avi_config_dict = f5_config_converter.convert(
-            f5_config_dict, ".."+os.path.sep+"output", "disable",
+            f5_config_dict, dir_path+os.path.sep+"output", "disable",
             "certs", "api-upload", 11)
-
-        with open('../output/ConversionStatus.csv') as csvfile:
+        with open(dir_path+'/output/ConversionStatus.csv') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 assert row['Status'] != 'error'
