e9d69f2bc6d01e637144d41e15c35c41c89e0ed2
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2016-06-21 13:25:22 +0530

    Added code for converting parsed tokens to dict

--- python/avi/netscaler_converter/netscaler_parser.py
+++ python/avi/netscaler_converter/netscaler_parser.py
@@ -1,4 +1,5 @@
 from pyparsing import *
+import ast
 
 
 def parse_config_file(filepath):
@@ -22,13 +23,42 @@ def parse_config_file(filepath):
 
     # line starts, anything follows until EOL, fails on blank lines,
     conf_line = (LineStart() + SkipTo(LineEnd()) + EOL).setParseAction(
-        parse_command).setDebug()
+        parse_command)
     grammar = OneOrMore(conf_line)
     grammar.ignore(comment | blank_line)
     parsed_str = grammar.parseString(file_str)
     return parsed_str
 
 
+def get_command(line):
+    commands = ['add server', 'add service', 'add lb vserver', 'bind lb vserver']
+    for command in commands:
+        cmd_arr = command.split(' ')
+        if line[0: len(cmd_arr)] == cmd_arr:
+            return command, len(cmd_arr)
+    return None, None
+
+
 if __name__ == "__main__":
-    result = parse_config_file("D:\\avi\\NetscalerConverter\\qualcom\\ns.conf")
-    print result
+    netscaler_conf = dict()
+
+    # result = parse_config_file("D:\\avi\\NetscalerConverter\\qualcom\\ns.conf")
+    result = parse_config_file("D:\\avi\\NetscalerConverter\\test.conf")
+    for line in result:
+        cmd, offset = get_command(line)
+        if cmd:
+            cmd_dict = dict()
+            attr_list = []
+            key = line[offset]
+            line = line[offset:]
+            print line
+            for token in line:
+                if isinstance(token, ParseResults):
+                    cmd_dict.update({token[0]: token[1]})
+                else:
+                    attr_list.append(token)
+            cmd_dict.update({'attrs':attr_list})
+            cmd_list = netscaler_conf.get(cmd, {})
+            cmd_list.update({key:cmd_dict})
+            netscaler_conf.update({cmd: cmd_list})
+    print netscaler_conf
