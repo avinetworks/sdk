fca787cf0e170dcd9b1db4fef867fcd079a5beab
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2016-07-05 17:43:56 +0530

    #AV-10970 : Added script to create deb and rpm packages for Avi SDK

---
+++ python/create_sdk_pkgs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd avi/sdk/
+dpkg-buildpackage -b -us -uc
+mv ../python-avisdk_*.deb ../../dist/
+rm -rf ../avisdk_*amd64.changes
+python setup.py bdist_rpm
+mv dist/avisdk*.noarch.rpm ../../dist/
+rm -rf avisdk.egg-info
+rm -rf build/
+rm -rf dist/
