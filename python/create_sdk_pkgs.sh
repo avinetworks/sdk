f00e443e4d6ccec8a900330171d5722705ba0286
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2016-07-06 12:15:43 +0000

    Added changes to update version in debian and create release

--- python/create_sdk_pkgs.sh
+++ python/create_sdk_pkgs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir -p dist
+
 cd avi/sdk/
+python debian/update_version.py
 dpkg-buildpackage -b -us -uc
 mv ../python-avisdk_*.deb ../../dist/
 rm -rf ../avisdk_*amd64.changes
@@ -8,4 +9,4 @@ python setup.py bdist_rpm
 mv dist/avisdk*.noarch.rpm ../../dist/
 rm -rf avisdk.egg-info
 rm -rf build/
-rm -rf dist/
+#rm -rf dist/
