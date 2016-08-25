b612e2ca7c6c42422ad846d9e0f896700b521a27
Gaurav Rastogi <grastogi@avinetworks.com>
2016-08-25 15:57:55 -0700

    changed the version to be pypi compliant

--- python/avi/version.py
+++ python/avi/version.py
@@ -5,8 +5,7 @@ Created on Apr 22, 2016
 '''
 
 AVI_VERSION = '16.3'
-AVI_PIP_VERSION = (AVI_VERSION if len(AVI_VERSION.split('.')) > 2
-                   else (AVI_VERSION + '.0'))
+AVI_PIP_VERSION = AVI_VERSION + 'b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
