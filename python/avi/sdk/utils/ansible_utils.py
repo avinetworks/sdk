a2927a84a1e3e58ebdbf4b0b981f549b3c2e36c0
Gaurav Rastogi <grastogi@avinetworks.com>
2016-10-06 00:37:59 -0700

    Added avi_ansible_api that hides away all the functionality regarding avi apis

--- python/avi/sdk/utils/ansible_utils.py
+++ python/avi/sdk/utils/ansible_utils.py
@@ -5,6 +5,8 @@ Created on Aug 16, 2016
 '''
 import re
 import logging
+from copy import deepcopy
+from avi.sdk.avi_api import ApiSession, ObjectNotFound
 log = logging.getLogger(__name__)
 
 
@@ -198,3 +200,64 @@ def avi_obj_cmp(x, y, sensitive_fields=None):
             if not avi_obj_cmp(v, y[k], sensitive_fields=sensitive_fields):
                 return False
     return True
+
+
+def avi_ansible_api(module, obj_type, sensitive_fields):
+    """
+    This converts the Ansible module into AVI object and invokes APIs
+    :param module: Ansible module
+    :param obj_type: string representing Avi object type
+    :param sensitive_fields: sensitive fields to be excluded for comparison
+        purposes.
+    Returns:
+        success: module.exit_json with obj=avi object
+        faliure: module.fail_json
+    """
+    api = ApiSession.get_session(
+            module.params['controller'],
+            module.params['username'],
+            module.params['password'],
+            tenant=module.params['tenant'])
+    state = module.params['state']
+    name = module.params['name']
+    obj = deepcopy(module.params)
+    obj.pop('state', None)
+    obj.pop('controller', None)
+    obj.pop('username', None)
+    obj.pop('password', None)
+    tenant = obj.pop('tenant', '')
+    tenant_uuid = obj.pop('tenant_uuid', '')
+    obj.pop('cloud_ref', None)
+    purge_optional_fields(obj, module)
+    if state == 'absent':
+        try:
+            rsp = api.delete_by_name(obj_type, name,
+                tenant=tenant, tenant_uuid=tenant_uuid)
+        except ObjectNotFound:
+            return module.exit_json(changed=False)
+        if rsp.status_code == 204:
+            return module.exit_json(changed=True)
+        return module.fail_json(msg=rsp.text)
+    existing_obj = api.get_object_by_name(
+        obj_type, name, tenant=tenant, tenant_uuid=tenant_uuid,
+        params={'include_refs': '', 'include_name': ''})
+    changed = False
+    rsp = None
+    if existing_obj:
+        # this is case of modify as object exists. should find out
+        # if changed is true or not
+        changed = not avi_obj_cmp(obj, existing_obj, sensitive_fields)
+        cleanup_absent_fields(obj)
+        if changed:
+            obj_uuid = existing_obj['uuid']
+            rsp = api.put('%s/%s' % (obj_type, obj_uuid), data=obj,
+                tenant=tenant, tenant_uuid=tenant_uuid)
+    else:
+        changed = True
+        rsp = api.post(obj_type, data=obj,
+                       tenant=tenant, tenant_uuid=tenant_uuid)
+    if rsp is None:
+        return module.exit_json(changed=changed, obj=existing_obj)
+    else:
+        return ansible_return(module, rsp, changed)
+
