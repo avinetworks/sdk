c708517bb2570a065e0c7b2e94452ce0e87d4c9e
Gaurav Rastogi <grastogi@avinetworks.com>
2017-02-24 12:46:11 -0800

    Added version to the sdk that can then be checked by caller

--- python/create_sdk_pip_packages.sh
+++ python/create_sdk_pip_packages.sh
@@ -8,7 +8,8 @@ fi
 cp avi/$1/setup.py .
 cp avi/$1/MANIFEST.in .
 AVI_PIP_VERSION=`python version.py`
-sed -i s/"AVI_PIP_VERSION=.*$"/"AVI_PIP_VERSION= \'$AVI_PIP_VERSION\'"/g setup.py
+sed -i s/"AVI_PIP_VERSION =.*$"/"AVI_PIP_VERSION = \'$AVI_PIP_VERSION\'"/g setup.py
+sed -i s/"__version__ =.*$"/"__version__ = \'$AVI_PIP_VERSION\'"/g avi/$1/__init__.py
 echo "copied setup and manifest to top level python directory"
 echo "creating package"
 python setup.py sdist
@@ -22,3 +23,4 @@ else
 fi
 rm -f setup.py
 rm -f MANIFEST.in
+sed -i s/"__version__ =.*$"/"__version__ = \'\'"/g avi/$1/__init__.py
