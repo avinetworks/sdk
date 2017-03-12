781f74c97ed9eb945281b3cfbc0a1ec396d75bf5
Ramesh Tathe <ramesh.tathe@avinetworks.com>
2017-03-12 15:10:41 +0530

    AV-18607 Changes includes: 1. Add INI file which will load into csv status test suite, has following parameters 	- input directory location(has multiple input file config) 	- output directory 	- tenant 	- cloud name 	- vs state 	- list of input config to upload output configuration on controller

--- python/avi/netscaler_converter/test/netscaler_e2e_test.py
+++ python/avi/netscaler_converter/test/netscaler_e2e_test.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python
-import argparse
 import os
 from ConfigParser import ConfigParser
 
 
-def load_ini(ini_file, output_dir):
-    """ Parse and collapse a ConfigParser-Style ini file into a nested,
+def set_output_dir_in_test_config_ini(ini_file, output_dir):
+    """
+    Parse and collapse a ConfigParser-Style ini file into a nested,
     eval'ing the individual values, as they are assumed to be valid
-    python statement formatted """
+    python statement formatted
+    :param ini_file: Path of INI file
+    :param output_dir: value of output_dir
+    :return: None
+    """
 
     ini_config = ConfigParser()
     ini_config.read(ini_file)
@@ -20,52 +23,56 @@ def load_ini(ini_file, output_dir):
     with open(ini_file, 'w') as configfile:  # save
         ini_config.write(configfile)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--ns_config_dir',
-                        help='absolute path for Netscaler config directory')
-    parser.add_argument('-l', '--input_folder_location',
-                        help='location of extracted backup folder')
-    parser.add_argument('-o', '--output_dir_path',
-                        help='Folder path for output files to be created in')
-    parser.add_argument('-t', '--tenant', help='tenant name for auto upload')
-    parser.add_argument('--cloud_name', help='cloud name for auto upload')
-    parser.add_argument('-s', '--vs_state', choices=['enable', 'disable'],
-                        help='state of VS created')
 
-    args = parser.parse_args()
+if __name__ == "__main__":
+    # set INI file path
+    test_config_ini_path = 'test/netscaler_e2e_test_cfg.ini'
+    # Read INI file
+    ini_config = ConfigParser()
+    ini_config.read(test_config_ini_path)
 
     # Raise exception if not provide input location
-    if not args.ns_config_dir:
+    if not ini_config.get('netscaler_e2e_config', 'ns_config_dir'):
         raise Exception('Please provide input directiry')
-    input_path = args.ns_config_dir
+    # Get the input folder location for config files
+    input_path = ini_config.get('netscaler_e2e_config', 'ns_config_dir')
     # File path of test config ini file which used for to
-    test_config_ini_path = 'test/csv_status_cfg.ini'
+    test_config_ini_path = 'test/netscaler_e2e_test_cfg.ini'
+    output_dir_path = ini_config.get('netscaler_e2e_config',
+                                     'output_dir_path') \
+        if ini_config.get('netscaler_e2e_config', 'output_dir_path') \
+        else input_path
+    # Get the list of files from input folder
     input_files = [config_file for config_file in os.listdir(input_path)
                    if os.path.isfile(os.path.join(input_path, config_file))]
     for input in input_files:
-        output_dir = args.output_dir_path if args.output_dir_path else input_path
+        # Set the input file to convert
         input_file = os.path.abspath(input_path + '/' + input)
-        output_dir = os.path.abspath(output_dir + '/' + input + '-output')
+        # Set the output directory path to be create
+        output_dir = os.path.abspath(output_dir_path + '/' + input + '-output')
+        # If outout directory not exist then create output directory
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
         output_dir += '/output'
-        run_script = 'python netscaler_converter.py -f %s -o %s ' % (input_file,
-                                                                    output_dir)
-        if args.tenant:
-            run_script += '-t %s ' % args.tenant
-        if args.input_folder_location:
-            run_script += '-l %s ' % args.input_folder_location
-        if args.cloud_name:
-            run_script += '--cloud_name %s ' % args.cloud_name
-        if args.vs_state:
-            run_script += '-s %s ' % args.vs_state
+        run_script = 'python netscaler_converter.py -f %s -o %s ' \
+                     % (input_file, output_dir)
+        if ini_config.get('netscaler_e2e_config', 'tenant'):
+            run_script += '-t %s ' % ini_config.get('netscaler_e2e_config',
+                                                    'tenant')
+        if ini_config.get('netscaler_e2e_config', 'cloud_name'):
+            run_script += '--cloud_name %s ' % \
+                          ini_config.get('netscaler_e2e_config', 'cloud_name')
+        if ini_config.get('netscaler_e2e_config', 'vs_state'):
+            run_script += '-s %s ' % ini_config.get('netscaler_e2e_config',
+                                                    'vs_state')
         # Start execution of netscaler_converter.py
         os.system(run_script)
-        # End execution of netscaler_converter.py
         # Location path for html log report
         test_report_location = '%s/log.html' % output_dir
-        load_ini(test_config_ini_path, output_dir)
-        os.system("nosetests test/test_csv_status.py -s --tc-file=%s --with-html "
-                  "--html-report=%s" % (test_config_ini_path, test_report_location))
-
+        # Set the output directory location in INI file which will be read by
+        # test config
+        set_output_dir_in_test_config_ini(test_config_ini_path, output_dir)
+        # Run test csv status test suite
+        os.system("nosetests test/test_csv_status.py -s --tc-file=%s "
+                  "--with-html --html-report=%s" % (test_config_ini_path,
+                                                    test_report_location))
