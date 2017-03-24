af78fce29481bded98c85e80b2e43d477ab29b05
Ramesh Tathe <ramesh.tathe@avinetworks.com>
2017-03-24 08:40:02 +0530

    Fixed review comments

--- python/avi/netscaler_converter/profile_converter.py
+++ python/avi/netscaler_converter/profile_converter.py
@@ -2,11 +2,10 @@ import logging
 import avi.netscaler_converter.ns_util as ns_util
 import os
 import re
-import yaml
 import avi.netscaler_converter.ns_constants as ns_constants
+
 from datetime import datetime
 from OpenSSL import crypto as c
-
 from avi.netscaler_converter.ns_constants import (STATUS_SKIPPED,
                                                   STATUS_SUCCESSFUL,
                                                   STATUS_INDIRECT,
@@ -20,7 +19,7 @@ tmp_pki_profile_list = []
 class ProfileConverter(object):
 
     def __init__(self, tenant_name, cloud_name, tenant_ref, cloud_ref,
-                 ssl_ciphers_yaml, keypassphrase=None):
+                 ssl_ciphers, keypassphrase=None):
         
         self.profile_http_skip = \
             ns_constants.netscalar_command_status['profile_http_skip']
@@ -72,11 +71,9 @@ class ProfileConverter(object):
         self.tenant_ref = tenant_ref
         self.cloud_ref = cloud_ref
         # ssl cipher yaml
-        ssl_ciphers_dict = yaml.safe_load(open(os.path.dirname(__file__) + '/%s'
-                                               % ssl_ciphers_yaml))
         self.netscaler_ssl_cipher_to_open_ssl_cipher = \
-            ssl_ciphers_dict.get('netscaler_ssl_cipher_to_open_ssl_cipher', {})
-        self.open_ssl_cipher_to_avi_ssl_cipher = ssl_ciphers_dict.get(
+            ssl_ciphers.get('netscaler_ssl_cipher_to_open_ssl_cipher', {})
+        self.open_ssl_cipher_to_avi_ssl_cipher = ssl_ciphers.get(
             'open_ssl_cipher_to_avi_ssl_cipher', {})
         # list of keys with passphrase provided in YAML.
         self.netscalar_passphrase_keys = None
