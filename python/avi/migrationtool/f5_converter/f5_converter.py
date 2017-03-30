f8bf1466c9012c5c7d4383f6bf5787e757059e35
Ramesh Tathe <ramesh.tathe@avinetworks.com>
2017-03-30 19:49:17 +0530

    Changes Include: 1. Fixes input directory path 2. Fixes virtual service skipped attributes settings. 3. Handeled unicode string in format json of ns util

--- python/avi/migrationtool/f5_converter/f5_converter.py
+++ python/avi/migrationtool/f5_converter/f5_converter.py
@@ -298,8 +298,8 @@ if __name__ == "__main__":
     parser.add_argument('--version',
                         help='Print product version and exit',
                         action='store_true')
-    parser.add_argument('--profilemerge',
-                        help='Falg for ssl profile merge', default=False)
+    parser.add_argument('--profilemerge', action='store_false',
+                        help='Falg for ssl profile merge', default=True)
     # Added command line args to execute config_patch file with related avi
     # json file location and patch location
     parser.add_argument('--patch', help='Run config_patch please provide args '
