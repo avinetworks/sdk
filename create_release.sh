a257cabfa2954a2822ddad716d1dc4f1505e673f
Gaurav Rastogi <grastogi@avinetworks.com>
2017-04-04 08:28:01 -0700

    Fixed the create release script to now publish avimigrationtools rather than individual converters

--- create_release.sh
+++ create_release.sh
@@ -37,8 +37,7 @@ do
 done
 
 ./create_sdk_pip_packages.sh sdk
-./create_sdk_pip_packages.sh f5_converter
-./create_sdk_pip_packages.sh netscaler_converter
+./create_sdk_pip_packages.sh migrationtools
 ./create_sdk_pypi.sh sdk
 ./create_sdk_pkgs.sh 
 
@@ -58,28 +57,20 @@ else
     exit 1
 fi
 
-if [ -e dist/avif5converter-$AVI_VERSION.tar.gz ]; then
-    mv dist/avif5converter-$AVI_VERSION.tar.gz ../avif5converter-$AVI_VERSION.tar.gz
+if [ -e dist/avimigrationtools-$AVI_VERSION.tar.gz ]; then
+    mv dist/avimigrationtools-$AVI_VERSION.tar.gz ../avimigrationtools-$AVI_VERSION.tar.gz
 else
-    echo "Avi F5 converter package not found. Aborting"
-    exit 1
-fi
-
-if [ -e dist/avinetscalerconverter-$AVI_VERSION.tar.gz ]; then
-    mv dist/avinetscalerconverter-$AVI_VERSION.tar.gz ../avinetscalerconverter-$AVI_VERSION.tar.gz
-else
-    echo "Avi Netscaler converter package not found. Aborting"
+    echo "Avi Migration tools package not found. Aborting"
     exit 1
 fi
 
 rm -rf dist
 rm -rf avisdk.egg-info
-assets="$assets -a avisdk-$AVI_VERSION.tar.gz#pip-package-avisdk-$AVI_VERSION -a avif5converter-$AVI_VERSION.tar.gz#pip-package-avif5converter-$AVI_VERSION -a avisdk-$AVI_VERSION.deb#debian-package-avisdk-$AVI_VERSION -a avisdk-$AVI_VERSION.rpm#rpm--package-avisdk-$AVI_VERSION -a avinetscalerconverter-$AVI_VERSION.tar.gz#pip-package-avinetscalerconverter-$AVI_VERSION"
+assets="$assets -a avisdk-$AVI_VERSION.tar.gz#pip-package-avisdk-$AVI_VERSION -a avimigrationtools-$AVI_VERSION.tar.gz#pip-package-avimigrationtools-$AVI_VERSION -a avisdk-$AVI_VERSION.deb#debian-package-avisdk-$AVI_VERSION -a avisdk-$AVI_VERSION.rpm#rpm--package-avisdk-$AVI_VERSION"
 cd ../
 
 /usr/local/bin/hub release $hub_op $assets -F ReleaseNote $REL_TAG
 rm -rf avisdk-$AVI_VERSION.tar.gz
-rm -rf avif5converter-$AVI_VERSION.tar.gz
-rm -rf avinetscalerconverter-$AVI_VERSION.tar.gz
+rm -rf avimigrationtools-$AVI_VERSION.tar.gz
 rm -rf avisdk-$AVI_VERSION.deb
 rm -rf avisdk-$AVI_VERSION.rpm
