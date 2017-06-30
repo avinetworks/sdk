731a5d10db6f2cb684e11014e0540d0eb60a70b1
Gaurav Rastogi <grastogi@avinetworks.com>
2017-06-30 08:04:51 -0700

    Changed the code to use cloud filter in get_object when cloud_ref is defined

--- python/avi/sdk/utils/ansible_utils.py
+++ python/avi/sdk/utils/ansible_utils.py
@@ -323,9 +323,11 @@ def avi_ansible_api(module, obj_type, sensitive_fields):
         if obj.get('cloud_ref', None):
             # this is the case when gets have to be scoped with cloud
             cloud = obj['cloud_ref'].split('name=')[1]
+            cloud_params = deepcopy(params)
+            cloud_params['fields'] = 'uuid'
             cloud_obj = api.get_object_by_name(
                 'cloud', cloud, tenant=tenant, tenant_uuid=tenant_uuid,
-                params=params, api_version=api_version)
+                params=cloud_params, api_version=api_version)
             if cloud_obj:
                 params['cloud_uuid'] = cloud_obj['uuid']
         existing_obj = api.get_object_by_name(
