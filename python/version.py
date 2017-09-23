25f39e6fbfdd4ee2c42c33049f17fe37f1221ed7
Gaurav Rastogi <grastogi@avinetworks.com>
2017-09-23 12:07:58 -0700

    updated version to 17.2.3b3

--- python/version.py
+++ python/version.py
@@ -5,7 +5,7 @@ Created on Apr 22, 2016
 '''
 
 AVI_VERSION = '17.2.3'
-AVI_PIP_VERSION = AVI_VERSION + 'b2'
+AVI_PIP_VERSION = AVI_VERSION + 'b3'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
