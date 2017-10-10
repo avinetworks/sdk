1deff1e046c6f2a562bbb679ed14e1d2c2120eac
Gaurav Rastogi <grastogi@avinetworks.com>
2017-10-10 08:12:50 -0700

    updated version to b10

--- python/version.py
+++ python/version.py
@@ -5,7 +5,7 @@ Created on Apr 22, 2016
 '''
 
 AVI_VERSION = '17.2.3'
-AVI_PIP_VERSION = AVI_VERSION + 'b8'
+AVI_PIP_VERSION = AVI_VERSION + 'b10'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
