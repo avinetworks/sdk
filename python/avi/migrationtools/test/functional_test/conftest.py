f6f0c2fe70491b74fcc12fe1493937bf712e85c5
patilshrikant486 <shrikant.patil@opcito.com>
2017-10-27 14:03:39 +0530

    Removed unused code and added comments to the functions

---
+++ python/avi/migrationtools/test/functional_test/conftest.py
@@ -0,0 +1,36 @@
+"""
+Get the configuration from the config.yml file.
+"""
+
+def pytest_addoption(parser):
+    parser.addoption("--bigip_config_file", action="store", help="config file")
+    parser.addoption("--type", action="store", help="config type")
+    parser.addoption("--controller_version", action="store", help=" Controller version", default="17.1.1" )
+    parser.addoption("--file_version", action="store", default="11")
+    parser.addoption("--vs_state", action="store", default='disable')
+    parser.addoption("--output_path", action="store", help="Output file path")
+    parser.addoption('--cloud_name', action="store", help='cloud name for auto upload',
+                         default='Default-Cloud')
+    parser.addoption('--input_folder_location',
+                         help='location of extracted backup folder')
+    parser.addoption('--key_file',
+                         help='host key file location if key based ' +
+                              'authentication')
+    parser.addoption('--passphrase_file',
+                         help='key passphrase yaml file path')
+    parser.addoption('--no_object_merge',
+                         help='Flag for object merge', action='store_false')
+    parser.addoption('--patch', help='Run config_patch please provide '
+                                         'location of patch.yaml')
+    parser.addoption('--vs_filter', help='comma seperated names of '
+                                             'virtualservices')
+    parser.addoption('--ignore_config',
+                         help='config json to skip the config in conversion')
+    parser.addoption('--prefix', help='Prefix for objects')
+    parser.addoption('--not_in_use',
+                         help='Flag for skipping not in use object',
+                         action="store_true")
+    parser.addoption('--baseline_profile', help='asolute path for json '
+                                                    'file containing baseline profiles')
+    parser.addoption('--ansible',
+                         help='Flag for create ansible file', action='store_true')
---
+++ python/avi/migrationtools/test/functional_test/test_migration.py
@@ -0,0 +1,264 @@
+import unittest
+import pytest
+import logging
+from avi.migrationtools.test.functional_test.f5_functional_test import *
+from avi.migrationtools.test.functional_test.F5_Conversion import F5Conversion
+from avi.migrationtools.f5_converter.conversion_util import F5Util
+import avi.migrationtools.test.functional_test.conversion_constant_util as conv_const
+
+conversion_util = F5Util()
+input_file = pytest.config.getoption("--bigip_config_file")
+output_file = pytest.config.getoption("--output_path")
+type = pytest.config.getoption("--type")
+controller_version = pytest.config.getoption("--controller_version")
+vs_state = pytest.config.getoption("--vs_state")
+f5_config_version = pytest.config.getoption("--file_version")
+
+logging.basicConfig(filename=output_file + '/config_check.log', level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+
+def setUpModule():
+    global f5_config_dict
+    global avi_config_dict
+    f5Cnv = F5Conversion()
+    f5_config_dict, avi_config_dict = f5Cnv.convert(
+        type, input_file, f5_config_version, output_file,
+        vs_state, controller_version)
+
+
+# Compare virtual service name with virtual service in f5 configuration.
+def compareVsName(vs_name):
+    """
+
+    :param: It contains a virtual service name
+    :return: return a virtual service name
+    """
+    vs_config = f5_config_dict.get("virtual", {})
+    for each_parsed_vs in vs_config.keys():
+        tenant, vsName = conversion_util.get_tenant_ref(each_parsed_vs)
+        if vsName == vs_name:
+            return vsName
+
+# Compares the application profile.
+def comAppProf(each_vs):
+    """
+
+    :param appType: It contains a application profile type
+    :return: It returns default profile type and avi application profile type
+    """
+
+    application_profile_ref = each_vs['application_profile_ref']
+    app_name = conversion_util.get_name(application_profile_ref)
+    app_type = [i['type'] for i in avi_config_dict['ApplicationProfile']
+                if i['name'] == app_name]
+    if app_type:
+        type = app_type[0]
+        for profile_type in conv_const.PROFILES.keys():
+            if conv_const.PROFILES[profile_type] == type:
+                return True
+    return False
+
+# Compare network profile.
+def compareNetworkProfile(each_vs):
+    """
+
+    :param each_vs: It contains virtual service information
+    :return networkProfile[0]: Type of default network profiles and avi network profiles
+    :return network_type[0]: Type of avi network profiles
+    """
+    network_profile_ref = each_vs['network_profile_ref']
+    network_name = conversion_util.get_name(network_profile_ref)
+    network_type = [i['profile']['type'] for i in avi_config_dict['NetworkProfile']
+                    if i['name'] == network_name]
+    if network_type and network_type[0] in conv_const.NETWORK_PROFILES:
+        return True
+    else: False
+
+
+""" Compare ssl profile type with
+    default profile types
+    """
+def compareSslProfile(each_vs):
+    """
+
+    :param each_vs: It contains virtual service information
+    :return: returns True if ssl profile type is matched with default profile types
+    """
+    ssl_profile_ref = each_vs['ssl_profile_ref']
+    ssl_profile_name = conversion_util.get_name(ssl_profile_ref)
+    ssl_profile_type = [i['accepted_versions'] for i in avi_config_dict['SSLProfile']
+                        if i['name'] == ssl_profile_name]
+    profile_types = [profile['type'] for profile in ssl_profile_type[0]]
+    return True if profile_types == conv_const.SSLPROFILE_VERSIONS else False
+
+
+# Checks health monitor in avi configurations HealthMonitor.
+def checkHealthMonitor(pool):
+    """
+
+    :param pool: It contains pool information.
+    :return matchedMonitor: It contains health monitor list of avi health monitors
+    :return monitors: It contains list of matched health monitors with F5 config
+    """
+    healthMonitorRef = pool['health_monitor_refs']
+    if healthMonitorRef:
+        healthMonitor = conversion_util.get_name(healthMonitorRef[0])
+        monitors = [healthMonitors['type'] for healthMonitors in avi_config_dict['HealthMonitor']
+                    if healthMonitors['name'] == healthMonitor]
+        matchedmMnitor = compareHealthMonitor(monitors, healthMonitor)
+        return matchedmMnitor, healthMonitor
+
+
+""" Compare health monitor type with
+    F5 configurations health monitors
+    """
+def compareHealthMonitor(healthMonitors, monitorName):
+    """
+
+    :param healthMonitors: List of health monitors.
+    :param monitorName: health monitor name.
+    :return healthMonitor: health monitors list.
+    """
+    monitor_config = f5_config_dict.get("monitor", {})
+    for key in monitor_config.keys():
+        if f5_config_version == "11":
+            monitorType, name = get_monitor_type(key)
+            type, name = conversion_util.get_tenant_ref(name)
+        elif f5_config_version == "10":
+            name = key
+        if monitorName == name or monitorName.startswith("Merged"):
+            for key in conv_const.HEALTH_MONITORS:
+                if conv_const.HEALTH_MONITORS[key] in healthMonitors:
+                    return True
+    return False
+
+
+# Return monitor type and name
+def get_monitor_type(monitor):
+    """
+
+    :param monitor: It contains key of health monitor
+    :return healthMonitor[0]: It contains type of health monitor
+    :return healthMonitor[1]: It contains name of health monitor
+
+    """
+    healthMonitor = monitor.split(' ')
+    return healthMonitor[0], healthMonitor[1]
+
+
+
+# Check persistence profile
+def checkPersistenceType(applicationProfile):
+    """
+
+    :param pool: It contains pool information.
+    :return: returns True if profile type is matched with default profile type.
+    """
+    profiles = [appProfile for appProfile in avi_config_dict['ApplicationPersistenceProfile']
+                        if appProfile['name'] == applicationProfile]
+    if profiles:
+        for key in conv_const.PERSISTENCE_PROFILE_TYPES:
+            if conv_const.PERSISTENCE_PROFILE_TYPES[key] == profiles[0]['persistence_type']:
+                return True,applicationProfile
+
+        return False,applicationProfile
+    else: return False,applicationProfile
+
+
+# Return profile application persistence profile name
+def getProfileName(pool):
+    """
+
+    :param pool: it contains pool information.
+    :return: returns profile name
+    """
+    profile = pool['application_persistence_profile_ref']
+    persistenceProfile = conversion_util.get_name(profile)
+    if not persistenceProfile.startswith("System"):
+        return persistenceProfile
+
+class Test(unittest.TestCase):
+    def test_compareVs(self):
+        for each_vs in avi_config_dict['VirtualService']:
+            try:
+                vs_name = each_vs['name']
+                f5VsName = compareVsName(vs_name)
+                assert f5VsName == vs_name
+            except AssertionError:
+                LOG.error(
+                    'Virtual service not found in F5 configuration : %s ' % (vs_name))
+
+            if f5VsName:
+                try:
+                    if 'application_profile_ref' in each_vs:
+                        profileStatus = comAppProf(each_vs)
+                        self.assertTrue(profileStatus)
+                except AssertionError:
+                    LOG.error("Application profile not found for : %s" %(vs_name))
+
+                try:
+                    if 'network_profile_ref' in each_vs:
+                        networkProfile = compareNetworkProfile(each_vs)
+                        self.assertTrue(networkProfile)
+                except AssertionError:
+                    LOG.error("Network profile is not found for %s" %(vs_name))
+
+                try:
+                    if 'ssl_profile_ref' in each_vs:
+                        isCompare = compareSslProfile(each_vs)
+                        self.assertTrue(isCompare)
+                except AssertionError:
+                    LOG.error(
+                        "SSL Profile not found in %s" % (vs_name))
+
+                if 'pool_group_ref' in each_vs:
+                    poolRef = each_vs['pool_group_ref']
+                    poolGroupName = conversion_util.get_name(poolRef)
+                    poolGroups = [poolGroup for poolGroup in avi_config_dict['PoolGroup']
+                                  if poolGroup['name'] == poolGroupName]
+                    poolMembers = [conversion_util.get_name(i['pool_ref'])
+                                   for i in poolGroups[0]['members']]
+                    for pool in avi_config_dict['Pool']:
+                        if pool['name'] in poolMembers:
+                            try:
+                                if 'application_persistence_profile_ref' in pool and pool['application_persistence_profile_ref']:
+                                    applicationProfile = getProfileName(pool)
+                                    if applicationProfile:
+                                        persistenceStatus, profileName = checkPersistenceType(applicationProfile)
+                                        self.assertTrue(persistenceStatus)
+                            except AssertionError:
+                                LOG.error("Application persistence profile %s not found in %s " % (profileName, vs_name))
+                            try:
+                                if 'health_monitor_refs' in pool and pool['health_monitor_refs']:
+                                    f5healthMonitorStatus, monitorName = checkHealthMonitor(pool)
+                                    self.assertTrue(f5healthMonitorStatus)
+                            except AssertionError:
+                                LOG.error("Health monitor %s not found for virtual service %s" % (monitorName, vs_name))
+
+                if 'pool_ref' in each_vs:
+                    poolRef = each_vs['pool_ref']
+                    poolName = conversion_util.get_name(poolRef)
+                    for pool in avi_config_dict['Pool']:
+                        if pool['name'] in poolName:
+                            try:
+                                if 'application_persistence_profile_ref' in pool and pool['application_persistence_profile_ref']:
+                                    applicationProfile = getProfileName(pool)
+                                    if applicationProfile:
+                                        persistenceStatus, profileName = checkPersistenceType(applicationProfile)
+                                        self.assertTrue(persistenceStatus)
+                            except AssertionError:
+                                LOG.error("Application persistence profile %s not found in %s " % (profileName, vs_name))
+                            try:
+                                if 'health_monitor_refs' in pool and pool['health_monitor_refs']:
+                                    f5healthMonitorStatus, monitorName = checkHealthMonitor(pool)
+                                    self.assertTrue(f5healthMonitorStatus)
+                            except AssertionError:
+                                LOG.error("Health monitor %s not found for virtual service %s" % (monitorName, vs_name))
+
+            else:
+                pass
+
+
+if __name__ == "__main__":
+    unittest.main()
