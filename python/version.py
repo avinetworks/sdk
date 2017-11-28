61a743b16735b2330b73e2b66d746663bf14a157
Chaitanya Deshpande <chaitanya.deshpande@opcito.com>
2017-11-28 14:54:19 +0530

    Changes for version updates

--- python/version.py
+++ python/version.py
@@ -4,8 +4,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '17.2.4'
-AVI_PIP_VERSION = '17.2.4'
+AVI_VERSION = '17.2.5'
+AVI_PIP_VERSION = '17.2.5'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
