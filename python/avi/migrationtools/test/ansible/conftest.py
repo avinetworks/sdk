c99231258e43de786380bba84ad9bdce58722de9
patilshrikant486 <shrikant.patil@opcito.com>
2018-02-01 18:47:14 +0530

    Updated conftest.py and added few testcases

--- python/avi/migrationtools/test/ansible/conftest.py
+++ python/avi/migrationtools/test/ansible/conftest.py
@@ -4,3 +4,4 @@ Get the configuration from the yml file.
 
 def pytest_addoption(parser):
     parser.addoption("--config", action="store", help="playbook file")
+    parser.addoption("--change", action="store", help="expected change")
