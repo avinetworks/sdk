c673e31ceabaa0a38495b91ad0d9f4a0262faae6
patilshrikant486 <shrikant.patil@opcito.com>
2018-02-07 21:53:36 +0530

    Updated .dat

--- python/avi/migrationtools/test/ansible/ansible_test.py
+++ python/avi/migrationtools/test/ansible/ansible_test.py
@@ -18,7 +18,8 @@ LOG = logging.getLogger(__name__)
 
 def test_ansible_configuration():
 
-        cmd = ansible_temp.substitute(playbook = playbook_file)
+    cmd = ansible_temp.substitute(playbook = playbook_file)
+    try:
         res = subprocess.check_output(cmd, shell= True)
         sout = StringIO(res)
         lines = sout.readlines()
@@ -38,28 +39,6 @@ def test_ansible_configuration():
                 LOG.error("Ansible module is faild to create configuration from %s" %(playbook_file))
             assert int(ch) == int(expected_change)
             LOG.info("Playbook %s uploaded successfully.." %(playbook_file))
-
-
-    # try:
-    #     res = subprocess.check_output(cmd, shell= True)
-    #     sout = StringIO(res)
-    #     lines = sout.readlines()
-    #
-    #     for index, line in enumerate(lines):
-    #         if line.startswith('TASK'):
-    #             result = lines[index + 1]
-    #             LOG.info(result)
-    #         if not line.startswith('PLAY RECAP'):
-    #             continue
-    #         results = lines[index + 1]
-    #         ch = [term.split('=')[1] for term in results.split()
-    #               if term.startswith('changed')][0]
-    #         err = [term.split('=')[1] for term in results.split()
-    #               if term.startswith('failed')][0]
-    #         if int(err) > 1:
-    #             LOG.error("Ansible module is faild to create configuration from %s" %(playbook_file))
-    #         assert int(ch) == int(expected_change)
-    #         LOG.info("Playbook %s uploaded successfully.." %(playbook_file))
-    # except subprocess.CalledProcessError as err:
-    #    print err.output
+    except subprocess.CalledProcessError as err:
+       print err.output
 
