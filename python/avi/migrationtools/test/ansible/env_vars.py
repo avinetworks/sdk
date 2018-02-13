2ff16ac4ec986850896ef3ee9470368317a5f85f
patilshrikant486 <shrikant.patil@opcito.com>
2018-02-13 14:00:22 +0530

    Added playbooks

--- python/avi/migrationtools/test/ansible/env_vars.py
+++ python/avi/migrationtools/test/ansible/env_vars.py
@@ -5,4 +5,4 @@ import os
 os.environ['AVI_USERNAME'] = "admin"
 os.environ['AVI_PASSWORD'] = "admin"
 os.environ['AVI_CONTROLLER'] = "10.10.28.66"
-os.environ['API_VERSION'] = "17.2.1"
+os.environ['API_VERSION'] = "17.2.5"
