5f1525b50be31afe232989d0566339e0c663be99
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2018-04-06 22:07:55 +0530

    Merge branch '17.2.8' of https://github.com/avinetworks/sdk into downmerge-17.2.8-to-master
    # Conflicts:Resolved
    #	python/version.py

--- python/version.py
+++ python/version.py
@@ -4,8 +4,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '17.2.9'
-AVI_PIP_VERSION = '17.2.9' + 'b1'
+AVI_VERSION = '17.2.8'
+AVI_PIP_VERSION = '17.2.8' + 'b4'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
