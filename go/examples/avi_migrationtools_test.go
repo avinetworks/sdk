6c31574bc7c83f3172a0c3cba354622d33b1c46a
patilshrikant486 <shrikant.patil@avinetworks.com>
2018-04-11 19:59:59 +0530

    Did changes to test on travis

--- go/examples/avi_migrationtools_test.go
+++ go/examples/avi_migrationtools_test.go
@@ -23,61 +23,61 @@ func TestCreateCloud(t *testing.T) {
 }
 
 
-func TestCreateTenant(t *testing.T) {
-
-	command := "go run create_tenant.go"
-	parts := strings.Fields(command)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-		t.Fail()
-	}
-}
-
-
-func TestCreateHealthMonitor(t *testing.T) {
-
-	command := "go run create_healthmonitor.go"
-	parts := strings.Fields(command)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-		t.Fail()
-	}
-}
-
-
-func TestCreateProfiles(t *testing.T) {
-
-	command := "go run create_profiles.go"
-	parts := strings.Fields(command)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-		t.Fail()
-	}
-}
-
-
-func TestCreateVirtualservice(t *testing.T) {
-
-	command := "go run create_virtualservice.go"
-	parts := strings.Fields(command)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-		t.Fail()
-	}
-}
+//func TestCreateTenant(t *testing.T) {
+//
+//	command := "go run create_tenant.go"
+//	parts := strings.Fields(command)
+//	cmd := exec.Command(parts[0], parts[1:]...)
+//	cmd.Stderr = os.Stderr
+//	cmd.Stdout = os.Stdout
+//	err := cmd.Run()
+//	if err != nil {
+//		panic(err)
+//		t.Fail()
+//	}
+//}
+//
+//
+//func TestCreateHealthMonitor(t *testing.T) {
+//
+//	command := "go run create_healthmonitor.go"
+//	parts := strings.Fields(command)
+//	cmd := exec.Command(parts[0], parts[1:]...)
+//	cmd.Stderr = os.Stderr
+//	cmd.Stdout = os.Stdout
+//	err := cmd.Run()
+//	if err != nil {
+//		panic(err)
+//		t.Fail()
+//	}
+//}
+//
+//
+//func TestCreateProfiles(t *testing.T) {
+//
+//	command := "go run create_profiles.go"
+//	parts := strings.Fields(command)
+//	cmd := exec.Command(parts[0], parts[1:]...)
+//	cmd.Stderr = os.Stderr
+//	cmd.Stdout = os.Stdout
+//	err := cmd.Run()
+//	if err != nil {
+//		panic(err)
+//		t.Fail()
+//	}
+//}
+//
+//
+//func TestCreateVirtualservice(t *testing.T) {
+//
+//	command := "go run create_virtualservice.go"
+//	parts := strings.Fields(command)
+//	cmd := exec.Command(parts[0], parts[1:]...)
+//	cmd.Stderr = os.Stderr
+//	cmd.Stdout = os.Stdout
+//	err := cmd.Run()
+//	if err != nil {
+//		panic(err)
+//		t.Fail()
+//	}
+//}
