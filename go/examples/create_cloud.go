172c92d9b95e04d93c070c792c58d430ea9bb51f
patilshrikant486 <shrikant.patil@avinetworks.com>
2018-04-11 14:28:00 +0530

    Added expamles to test

--- go/examples/create_cloud.go
+++ go/examples/create_cloud.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+
 	"github.com/avinetworks/sdk/go/clients"
 	"github.com/avinetworks/sdk/go/models"
 	"github.com/avinetworks/sdk/go/session"
+	"fmt"
 )
 
 
 func main() {
+
 	aviClient, err := clients.NewAviClient("10.10.28.91", "admin",
 		session.SetPassword("avi123"),
 		session.SetTenant("admin"),
@@ -20,17 +22,19 @@ func main() {
 	}
 	cv, err := aviClient.AviSession.GetControllerVersion()
 	fmt.Printf("Avi Controller Version: %v:%v\n", cv, err)
+
 	cloudobj := models.Cloud{}
-    cloudobj.Name = "Test-vcenter-cloud"
-    cloudobj.Vtype = "CLOUD_NONE"
-    cloudobj.EnableVipStaticRoutes = false
-    cloudobj.DhcpEnabled = false
-    cloudobj.LicenseType = "LIC_CORES"
-    cloudobj.LicenseTier = "ENTERPRISE_18"
-    cloudobj.ApicMode = false
-    cloudobj.Mtu = 1300
+	cloudobj.Name = "Test-vcenter-cloud"
+	cloudobj.Vtype = "CLOUD_NONE"
+	cloudobj.EnableVipStaticRoutes = false
+	cloudobj.DhcpEnabled = false
+	cloudobj.LicenseType = "LIC_CORES"
+	cloudobj.LicenseTier = "ENTERPRISE_18"
+	cloudobj.ApicMode = false
+	cloudobj.Mtu = 1300
 
 	tobj, err := aviClient.Cloud.Create(&cloudobj)
+
 	var obj interface{}
 	err = aviClient.AviSession.GetObjectByName("tenant", "webapp", &obj)
 	if err == nil {
