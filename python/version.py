6f2a76abd6192942392c569342a3d42934ec912c
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2018-05-25 12:13:27 +0530

    Initial commit for 17.2.12 braanch

--- python/version.py
+++ python/version.py
@@ -4,8 +4,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '17.2.11'
-AVI_PIP_VERSION = '17.2.11' + 'b1'
+AVI_VERSION = '17.2.12'
+AVI_PIP_VERSION = '17.2.12' + 'b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
