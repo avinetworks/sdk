9c06e76012ed3a26ac638966a20abd9b586a2932
Chaitanya Deshpande <15983253+chaitanyaavi@users.noreply.github.com>
2018-06-07 20:14:48 +0530

    AV-40311: Fix for lb algorithm miss-match (#658)
    * AV-40311: Fix for lb algorithm missmatch
    
    * AV-40311: Fix for lb algorithm missmatch
    
    * AV-40311: Fix for lb algorithm missmatch - added unit tests

--- python/avi/migrationtools/netscaler_converter/lbvs_converter.py
+++ python/avi/migrationtools/netscaler_converter/lbvs_converter.py
@@ -154,6 +154,10 @@ class LbvsConverter(object):
                 pool_group_ref = None
                 if pool_group:
                     pool_group_ref = pool_group_name
+                    ns_algo = lb_vs.get('lbMethod', 'LEASTCONNECTIONS')
+                    algo = ns_util.get_avi_lb_algorithm(ns_algo)
+                    ns_util.update_algo_for_pools(algo, pool_group_name, avi_config)
+
                 redirect_url = lb_vs.get('redirectURL', None)
                 backup_server = lb_vs.get('backupVServer', None)
                 updated_vs_name = re.sub('[:]', '-', vs_name)
@@ -194,6 +198,7 @@ class LbvsConverter(object):
                     'enabled': enabled,
                     'services': [],
                 }
+
                 if vrf:
                     vrf_ref = ns_util.get_object_ref(
                         vrf, 'vrfcontext', tenant=self.tenant_name,
