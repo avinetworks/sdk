d7c87945d38d8a5e850e873747bc0599153c576e
Shrikant Chaudhari <shrikant.chaudhari@avinetworks.com>
2018-09-25 11:03:18 +0530

    Modified check for response code

--- python/avi/sdk/avi_api.py
+++ python/avi/sdk/avi_api.py
@@ -469,13 +469,12 @@ class ApiSession(Session):
                              self.avi_credentials.username)
                 return
             # Check for bad request and invalid credentials response code
-            elif rsp.status_code >= 400 and rsp.status_code < 500:
+            elif rsp.status_code in [401, 403]:
                 logger.error('Status Code %s msg %s' % (
                     rsp.status_code, rsp.text))
                 err = APIError('Status Code %s msg %s' % (
                     rsp.status_code, rsp.text), rsp)
                 raise err
-                return
             else:
                 logger.error("Error status code %s msg %s", rsp.status_code,
                              rsp.text)
