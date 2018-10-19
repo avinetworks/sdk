35cf224349538a13a96f8af35cf0aeb5ea0dcff5
shrikant patil <shrikant.patil@avinetworks.com>
2018-10-19 17:08:04 +0530

    Update test_migrationtool.py

--- python/avi/migrationtools/netscaler_converter/test/test_migrationtool.py
+++ python/avi/migrationtools/netscaler_converter/test/test_migrationtool.py
@@ -370,7 +370,7 @@ class TestNetscalerConverter:
                        controller_version=setup.get('controller_version_v17'))
 
         assert get_count('error') == 0
-        assert get_count('warning') == 1
+        assert get_count('warning') == 5
 
     @pytest.mark.travis
     def test_lb_algorithm_match(self):
