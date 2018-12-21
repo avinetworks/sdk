dd42c9624b1552637eff846e788a29a387e29314
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2018-12-21 20:04:36 +0530

    Corrected version

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '18.2.1b1'
-AVI_PIP_VERSION = '18.2.1b1'
+AVI_VERSION = '18.2.1'
+AVI_PIP_VERSION = '18.2.1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
