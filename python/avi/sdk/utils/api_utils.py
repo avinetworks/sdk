7bc48053695f01caf2a626782aeb3b87710bb4b3
Eric Anderson <ericsysmin@users.noreply.github.com>
2018-12-21 09:55:42 -0800

    allow controller as entity_type

--- python/avi/sdk/utils/api_utils.py
+++ python/avi/sdk/utils/api_utils.py
@@ -76,7 +76,10 @@ class ApiUtils(object):
         if entity_name and not entity_uuid:
             resp = self.api.get_object_by_name(entity_type, entity_name)
             entity_uuid = self.api.get_obj_uuid(resp)
-        path = 'analytics/metrics/%s/%s' % (entity_type, entity_uuid)
+        if entity_type == 'controller':
+            path = 'analytics/metrics/%s' % entity_type
+        else:
+            path = 'analytics/metrics/%s/%s' % (entity_type, entity_uuid)
         if type(metric_id) == list:
             metric_id = ','.join(metric_id)
         if metric_id:
