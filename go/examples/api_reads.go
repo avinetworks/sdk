6ac766e16214d63d43dfb0a35c996c47d743d4c8
shubhamavi <shubham@avinetworks.com>
2019-01-18 00:54:13 +0530

    comments and go profiling

--- go/examples/api_reads.go
+++ go/examples/api_reads.go
@@ -18,7 +18,14 @@ var AVI_VIRTUALSERVICE_NAME = os.Getenv("")
 var AVI_AUTH_TOKEN = os.Getenv("AVI_AUTH_TOKEN")
 var AVI_API_ITERATIONS int
 var RESET_SESSION_AT_ITERATION int
-var SLEEP_TIME int // seconds
+var SLEEP_TIME int
+
+/*
+	global variable specifications
+	AVI_API_ITERATIONS:	number of iterations to run REST API tests
+	RESET_SESSION_AT_ITERATION: reset session after every n APIs
+	SLEEP_TIME: Use SLEEP_TIME to pause an iteration for n seconds
+*/
 
 func checkTime(start time.Time, testcase string, iteration int) {
 	now := time.Now()
@@ -40,10 +47,25 @@ func initialize_session(controller string, username string, tenant string, passw
 	}
 }
 
+/*
+	resource: https://flaviocopes.com/golang-profiling/
+	Setup profiling:
+		1. brew install graphviz (macOS) OR sudo apt-get install graphviz (linux)
+		2. go get github.com/pkg/profile
+			add "github.com/pkg/profile in the import object"
+		3. uncomment `defer profile.Start().Stop()`
+		4. go build api_reads.go
+
+	Running the program initiates CPU Profiling and generates a *.pprof file in a tmp directory
+	graphviz is a utility which convert .pprof file in a readable format
+		4. go tool pprof --pdf api_reads <path to .pprof file>  > pprof.pdf
+*/
 func main() {
+	// defer profile.Start().Stop()
+	// defer profile.Start(profile.MemProfile).Stop()
+
 	// flag.Lookup("logtostderr").Value.Set("false")
 	// Create a session and a generic client to Avi Controller
-
 	if AVI_USERNAME == "" {
 		AVI_USERNAME = "admin"
 	}
@@ -92,6 +114,7 @@ func main() {
 		var err error
 
 		if RESET_SESSION_AT_ITERATION != 0 && (i%RESET_SESSION_AT_ITERATION) == 0 && i != 0 {
+			glog.Infof("Resetting session\n")
 			avisess, err = initialize_session(AVI_CONTROLLER, AVI_USERNAME, AVI_TENANT, AVI_PASSWORD,
 				AVI_AUTH_TOKEN, aviVersion)
 		}
