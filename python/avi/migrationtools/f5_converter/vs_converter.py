9c7352b56f81113c8998284f1e6c6d8c0da08186
Chaitanya Deshpande <15983253+chaitanyaavi@users.noreply.github.com>
2019-02-27 12:18:55 +0530

    #AV-55235: [Test-Automation]: Changes for rate limit profile (#853)
    * #AV-55235: [Test-Automation]: Pool object needs to update - Fixed
    
    * #AV-55235: [Test-Automation]: Changes for rate limit profile for VS

--- python/avi/migrationtools/f5_converter/vs_converter.py
+++ python/avi/migrationtools/f5_converter/vs_converter.py
@@ -499,7 +499,15 @@ class VSConfigConv(object):
         rate_limit = int(f5_vs.get('rate-limit', '0'))
         if rate_limit > 0:
             vs_obj["connections_rate_limit"] = {
-                "count": rate_limit
+                "count": rate_limit,
+                'explicit_tracking': False,
+                'period': 1,
+                'action': {
+                    'status_code': 'HTTP_LOCAL_RESPONSE_STATUS_CODE_429',
+                    'type': "RL_ACTION_NONE"
+                },
+                'burst_sz': 0,
+                'fine_grain': False
             }
 
         if realm:
