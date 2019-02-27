890268802c5e0f563d2cce776b8a11494267b8cc
Chaitanya Deshpande <15983253+chaitanyaavi@users.noreply.github.com>
2019-02-27 12:18:55 +0530

    #AV-55235: [Test-Automation]: Changes for rate limit profile (#853)
    * #AV-55235: [Test-Automation]: Pool object needs to update - Fixed
    
    * #AV-55235: [Test-Automation]: Changes for rate limit profile for VS

--- python/avi/migrationtools/f5_converter/pool_converter.py
+++ python/avi/migrationtools/f5_converter/pool_converter.py
@@ -178,7 +178,15 @@ class PoolConfigConv(object):
                 limits['connection_limit']
         if limits.get('rate_limit', 0) > 0:
             pool_obj['max_conn_rate_per_server'] = {
-                'count': limits['rate_limit']
+                'count': limits['rate_limit'],
+                'explicit_tracking': False,
+                'period': 1,
+                'action': {
+                    'status_code': 'HTTP_LOCAL_RESPONSE_STATUS_CODE_429',
+                    'type': "RL_ACTION_NONE"
+                },
+                'burst_sz': 0,
+                'fine_grain': False
             }
         return pool_obj
 
