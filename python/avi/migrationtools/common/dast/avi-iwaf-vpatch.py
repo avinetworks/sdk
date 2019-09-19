5b64c9fb27f9476fea62b368426999edfc29efc8
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2019-09-19 15:19:40 +0530

    #AV-70249: Move dast WAF patch script to migration tools and add dependency

--- python/avi/migrationtools/common/dast/avi-iwaf-vpatch.py
+++ python/avi/migrationtools/common/dast/avi-iwaf-vpatch.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+if sys.version_info < (3, 5):
+    sys.stderr.write("python 3.5 or newer is required\n")
+    sys.exit(1)
+
 import argparse
 import json
-if sys.version_info[0] < 3:
-    from urlparse import urlparse
-else:
-    from urllib.parse import urlparse
+from urllib.parse import urlparse
 from enum import Enum
 import logging
 import datetime
@@ -322,7 +323,7 @@ def main():
             if args.verbose is True:
                 LOGGER.info(msg)
             else:
-                LOGGER.info(msg + " or --verbose to display config changes")
+                LOGGER.info(msg + "or --verbose to display config changes")
             return
         with ApiSession.get_session(args.controller,
                                     args.username,
