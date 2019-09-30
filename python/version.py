9284b7eea319e6d661f1cd2a5ab591930db097ff
karthikvas <karthik@avinetworks.com>
2019-09-30 12:00:39 +0000

    Updating the version file with 45.1.1

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '18.2.6'
-AVI_PIP_VERSION = '18.2.6b4'
+AVI_VERSION = '45.1.1'
+AVI_PIP_VERSION = '45.1.1b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
