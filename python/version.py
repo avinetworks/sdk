6be1d4e7214e11b6ce808ee4b212bae700b61d49
karthikvas <karthik@avinetworks.com>
2019-12-30 17:23:14 +0000

    Updating the version file with 18.2.8

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '18.2.7'
-AVI_PIP_VERSION = '18.2.7'
+AVI_VERSION = '18.2.8'
+AVI_PIP_VERSION = '18.2.8b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
