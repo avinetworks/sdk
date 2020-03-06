f49d4b44af02803eb8a13626780e25134331edbc
Ashwini Bodke <ashwini.bodke@avinetworks.com>
2020-03-06 10:32:12 +0530

    Java SDK thread safety and multipart upload download support and test changes  (#1099)
    * Added support for file upload and export configuration
    
    Done some modifications in AviApi and few test cases
    
    Input json file
    
    Refactored code for naming conventions and best practices.
    
    Added support for file upload and export configuration
    
    Done some modifications in AviApi and few test cases
    
    Refactored code for naming conventions and best practices.
    
    Added few changes for travis.yml
    
    code changes made to make download file functionality generic.
    
    Added support for file upload and export configuration
    
    Done some modifications in AviApi and few test cases
    
    Refactored code for naming conventions and best practices.
    
    Added support for file upload and export configuration
    
    Done some modifications in AviApi and few test cases
    
    Refactored code for naming conventions and best practices.
    
    code changes made to make download file functionality generic.
    
    code refactired for file download
    
    multipart file download support added & additional test cases written for the same
    
    null check added for file downloa
    
    added readme.md and latest jar into the target
    
    added readme.md and latest jar into the target
    
    Did some modifications in test cases
    
    * Fixed review comments
    
    * Fixed review comments
    
    Co-authored-by: abodke <abodke@vmware.com>

--- java/test/com/vmware/avi/sdk/AviSDKTest.java
+++ java/test/com/vmware/avi/sdk/AviSDKTest.java
@@ -4,26 +4,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 
 import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Properties;
-import java.util.logging.Logger;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.logging.Logger;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import com.vmware.avi.sdk.AviApi;
-import com.vmware.avi.sdk.AviApiException;
-import com.vmware.avi.sdk.AviCredentials;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
-import org.junit.Rule;
 
 public class AviSDKTest {
 	static final Logger LOGGER = Logger.getLogger(AviSDKTest.class.getName());
@@ -50,27 +42,16 @@ public class AviSDKTest {
 	}
 	
 	@Test
-	public void testExample() throws ParseException, AviApiException{
+	public void testExample() throws  Exception{
 		try {
 
 			AviApi serv = new AviApi(AviSDKTest.getCreds());
+			String content = readFile("InputFile.json", StandardCharsets.UTF_8);
+			JSONObject obj = new JSONObject(content);
+			JSONObject postInput = (JSONObject) obj.get("postInput");
+			String poolStr = postInput.toString();
 			
-			String poolStr = "{\n" + 
-					"	\"lb_algorithm\": \"LB_ALGORITHM_LEAST_CONNECTIONS\",\n" + 
-					"	\"default_server_port\": 80,\n" + 
-					"	\"enabled\": true,\n" + 
-					"	\"servers\": [{\n" + 
-					"		\"enabled\": false,\n" + 
-					"		\"ip\": {\n" + 
-					"			\"addr\": \"1.2.3.4\",\n" + 
-					"			\"type\": \"V4\"\n" + 
-					"		}\n" + 
-					"	}],\n" + 
-					"	\"name\": \"test-example\"\n" + 
-					"}";
-
-			JSONParser parser = new JSONParser();
-			JSONObject body = (JSONObject) parser.parse(poolStr);
+			JSONObject body = new JSONObject(poolStr);
 			JSONObject response = serv.post("pool", body);
 			String objectName = response.get("name").toString();
 			
@@ -93,7 +74,7 @@ public class AviSDKTest {
 			JSONObject request = serv.get("pool",value);
 			JSONArray res = (JSONArray) request.get("results");
             JSONObject putResult= (JSONObject)res.get(0);
-            putResult.replace("name", "test-pool");
+            putResult.put("name", "test-pool");
             
 			JSONObject updateRes = serv.put("pool", result);
             String updatedName  = (String)updateRes.get("name");
@@ -105,8 +86,59 @@ public class AviSDKTest {
 			String msg = deleteRes.get("Message").toString();
 			assertEquals("Object deleted successfully", "Object deleted successfully", msg);
 
-		} catch (AviApiException | ParseException e) {
+		} catch (AviApiException  e) {
 			e.printStackTrace(System.err);
 		}
-	}		
+	}
+	@Test
+	public void testPostFileUpload() throws Exception {
+		try {
+			AviApi serv = new AviApi(AviSDKTest.getCreds());
+			serv.fileUpload("fileservice/hsmpackages?hsmtype=safenet", "/mnt/files/hsmpackages/safenet.tar",
+					"controller://hsmpackages");
+		} catch (AviApiException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
+	@Test
+	public void downloadTextFile() throws Exception {
+		try {
+			AviApi serv = new AviApi(AviSDKTest.getCreds());
+			Map<String, String> param = new HashMap<String, String>();
+			param.put("full_system", "true");
+			param.put("passphrase", "abc1234");
+			serv.fileDownload("/configuration/export", "/tmp/file.txt", param);
+		} catch (AviApiException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
+	@Test
+	public void downloadJsonFile() throws Exception {
+		try {
+			AviApi serv = new AviApi(AviSDKTest.getCreds());
+			Map<String, String> param = new HashMap<String, String>();
+			param.put("passphrase", "abc1234");
+			serv.fileDownload("/configuration/export", "/tmp/file.json", param);
+		} catch (AviApiException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+	@Test
+	public void downloadTarFile() throws Exception {
+		try {
+			AviApi serv = new AviApi(AviSDKTest.getCreds());
+			Map<String, String> param = new HashMap<String, String>();
+			param.put("uri", "controller://tech_support/portal.20200225-082451.tar.gz");
+			serv.fileDownload("/fileservice", "/tmp/file.tar", param);
+		} catch (AviApiException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	public static String readFile(String path, Charset encoding) throws IOException {
+		List<String> lines = Files.readAllLines(Paths.get(path), encoding);
+		return String.join(System.lineSeparator(), lines);
+	}
 }
