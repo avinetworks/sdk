c28ba94c15c6d980456eb25d45947eab9e44e303
Yograj Shisode <yograj.shisode@avinetworks.com>
2020-03-17 20:06:34 +0530

    AV-70462 Added change to wait for the controller ready state (#1103)
    * AV-70462 Added change to wait for the controller ready state
    
    * AV-70462 Added test case and minor changes for to wait for the controller ready state
    
    * AV-70462 Fixed review comments
    
    * Fixed some review comments
    
    * Added test case for password update
    
    * Added changes to wait if controller is not in ready state

--- go/session/avisession.go
+++ go/session/avisession.go
@@ -434,33 +434,39 @@ func (avisess *AviSession) restRequest(verb string, uri string, payload interfac
 		return result, errorResult
 	}
 
+	retryReq := false
 	resp, err := avisess.client.Do(req)
 	if err != nil {
-		errorResult.err = fmt.Errorf("client.Do uri %v failed: %v", uri, err)
-		dump, err := httputil.DumpRequestOut(req, true)
-		debug(dump, err)
-		return result, errorResult
-	}
-	glog.Infof("Req for uri %v RespCode %v", url, resp.StatusCode)
-
-	errorResult.HttpStatusCode = resp.StatusCode
-	avisess.collectCookiesFromResp(resp)
-
-	retryReq := false
-	if resp.StatusCode == 401 && len(avisess.sessionid) != 0 && uri != "login" {
-		resp.Body.Close()
-		glog.Infof("Retrying url %s; retry %d due to Status Code %d", url, retry, resp.StatusCode)
-		err := avisess.initiateSession()
-		if err != nil {
-			return nil, err
+		// Wait untill controller is in ready state
+		if strings.Contains(err.Error(), "connection refused") {
+			retryReq = true
+		} else {
+			errorResult.err = fmt.Errorf("client.Do uri %v failed: %v", uri, err)
+			dump, err := httputil.DumpRequestOut(req, true)
+			debug(dump, err)
+			return result, errorResult
 		}
-		retryReq = true
-	} else if resp.StatusCode == 419 || (resp.StatusCode >= 500 && resp.StatusCode < 599) {
-		resp.Body.Close()
-		retryReq = true
-		glog.Infof("Retrying url%s; retry %d due to Status Code %d", url, retry, resp.StatusCode)
 	}
 
+	if !retryReq {
+		glog.Infof("Req for uri %v RespCode %v", url, resp.StatusCode)
+		errorResult.HttpStatusCode = resp.StatusCode
+		avisess.collectCookiesFromResp(resp)
+
+		if resp.StatusCode == 401 && len(avisess.sessionid) != 0 && uri != "login" {
+			resp.Body.Close()
+			glog.Infof("Retrying url %s; retry %d due to Status Code %d", url, retry, resp.StatusCode)
+			err := avisess.initiateSession()
+			if err != nil {
+				return nil, err
+			}
+			retryReq = true
+		} else if resp.StatusCode == 419 || (resp.StatusCode >= 500 && resp.StatusCode < 599) {
+			resp.Body.Close()
+			retryReq = true
+			glog.Infof("Retrying url%s; retry %d due to Status Code %d", url, retry, resp.StatusCode)
+		}
+	}
 	if retryReq {
 		check, err := avisess.CheckControllerStatus()
 		if check == false {
