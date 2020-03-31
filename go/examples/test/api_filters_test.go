e3dcd45484a3512bbb2185bae86014f0cddd2f37
Shubham Chauhan <shubham@avinetworks.com>
2020-03-31 14:40:19 +0530

    AV-81062: AviCollectionResult add Next field for next url
    collection result unit test
    
    Update api_filters_test.go
    
    Update api_filters_test.go
    
    Update api_filters_test.go
    
    include page
    
    fix webservice
    
    final

--- go/examples/test/api_filters_test.go
+++ go/examples/test/api_filters_test.go
@@ -39,13 +39,47 @@ func TestApiFilters(t *testing.T) {
 	}
 
 	params1 := map[string]string{
-		"page":      "1",
-		"tenant":    "admin",
+		"page":   "1",
+		"tenant": "admin",
 	}
 	data, err := aviClient.HealthMonitor.GetObject(session.SetParams(params1), session.SetName("Test-Hm"))
 	if err != nil {
 		fmt.Println("\n [ERROR] : ", err)
 	} else {
 		fmt.Println("\n Health monitor object  : ", *data)
+	}
+}
+
+func TestApiCollections(t *testing.T) {
+	aviClient, err := clients.NewAviClient(os.Getenv("controller"), "admin",
+		session.SetPassword(os.Getenv("password")),
+		session.SetTenant("admin"),
+		session.SetVersion(os.Getenv("version")),
+		session.SetInsecure)
+
+	if err != nil {
+		fmt.Println("Couldn't create session: ", err)
+		t.Fail()
+	}
+	cv, err := aviClient.AviSession.GetControllerVersion()
+	fmt.Printf("Avi Controller Version: %v:%v\n", cv, err)
+
+	params := map[string]string{
+		"page":      "1",
+		"page_size": "1",
+		"tenant":    "admin",
+	}
+
+	url := "api/healthmonitor"
+	hmCollectionData, err := aviClient.AviSession.GetCollectionRaw(url, session.SetParams(params))
+	if err != nil {
+		fmt.Println("\n [ERROR] : ", err)
+		t.Fail()
+	}
+	if hmCollectionData.Next != "" {
+		fmt.Println("\n Health Monitor page_size=1 Next url string : ", hmCollectionData.Next)
+	} else {
+		fmt.Println("\n Health Monitor Next url string not found")
+		t.Fail()
 	}
 }
