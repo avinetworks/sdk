31a9e2219bc41327ddf47d443b028d325cd12d3e
Rohan Suryawanshi <rohansuryawanshi90@gmail.com>
2020-05-11 18:12:45 +0530

    Revert "Updated go clients and models form eng (#1153)"
    This reverts commit 523b2466065df6e30cd40dc948143b1a5713f436.

--- go/models/httpsecurity_action.go
+++ go/models/httpsecurity_action.go
@@ -7,7 +7,7 @@ package models
 // swagger:model HTTPSecurityAction
 type HttpsecurityAction struct {
 
-	// Type of the security action to perform. Enum options - HTTP_SECURITY_ACTION_CLOSE_CONN, HTTP_SECURITY_ACTION_SEND_RESPONSE, HTTP_SECURITY_ACTION_ALLOW, HTTP_SECURITY_ACTION_REDIRECT_TO_HTTPS, HTTP_SECURITY_ACTION_RATE_LIMIT, HTTP_SECURITY_ACTION_REQUEST_CHECK_ICAP.
+	// Type of the security action to perform. Enum options - HTTP_SECURITY_ACTION_CLOSE_CONN, HTTP_SECURITY_ACTION_SEND_RESPONSE, HTTP_SECURITY_ACTION_ALLOW, HTTP_SECURITY_ACTION_REDIRECT_TO_HTTPS, HTTP_SECURITY_ACTION_RATE_LIMIT.
 	// Required: true
 	Action *string `json:"action"`
 
@@ -17,11 +17,8 @@ type HttpsecurityAction struct {
 	// Secure SSL/TLS port to redirect the HTTP request to. Allowed values are 1-65535.
 	HTTPSPort *int32 `json:"https_port,omitempty"`
 
-	// Rate Limit profile to be used to rate-limit the flow.  (deprecated). Field deprecated in 18.2.9.
+	// Rate Limit profile to be used to rate-limit the flow.
 	RateLimit *RateProfile `json:"rate_limit,omitempty"`
-
-	// Rate limiting configuration for this action. Field introduced in 18.2.9.
-	RateProfile *HttpsecurityActionRateProfile `json:"rate_profile,omitempty"`
 
 	// HTTP status code to use for local response. Enum options - HTTP_LOCAL_RESPONSE_STATUS_CODE_200, HTTP_LOCAL_RESPONSE_STATUS_CODE_204, HTTP_LOCAL_RESPONSE_STATUS_CODE_403, HTTP_LOCAL_RESPONSE_STATUS_CODE_404, HTTP_LOCAL_RESPONSE_STATUS_CODE_429, HTTP_LOCAL_RESPONSE_STATUS_CODE_501.
 	StatusCode *string `json:"status_code,omitempty"`
