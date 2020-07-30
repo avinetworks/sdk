8048cc1e6cbebe7ebac2d2785041d2e921112ecc
vijay-avi <vishankar@vmware.com>
2020-07-30 17:14:56 +0000

    Updating the version file with 20.1.2

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '20.1.1'
-AVI_PIP_VERSION = '20.1.1'
+AVI_VERSION = '20.1.2'
+AVI_PIP_VERSION = '20.1.2b1'
 
 if __name__ == '__main__':
     print(AVI_PIP_VERSION)
