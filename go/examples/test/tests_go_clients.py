d6439ec383e5d185a35119bd094928f4f8c58408
Yograj Shisode <yograj.shisode@avinetworks.com>
2020-08-06 15:35:21 +0530

    Removed duplicate test case

--- go/examples/test/tests_go_clients.py
+++ go/examples/test/tests_go_clients.py
@@ -94,18 +94,10 @@ def test_avi_client_error():
     out = subprocess.check_output(shlex.split(cmd))
     LOG.info("playbook out %s ", out)
 
+
 @pytest.mark.TCID1_48_1606_9_0
 def test_fileupload():
     cmd = PLAYBOOK_TEMPLATE.substitute(test_file='fileupload_test.go')
-    LOG.info("executing command %s ", cmd)
-    out = subprocess.check_output(shlex.split(cmd))
-    LOG.info("playbook out %s ", out)
-
-def test_avi_client_error():
-    '''
-    Test avi go sdk client with lazyauthentication and invalid controlleer IP
-    '''
-    cmd = PLAYBOOK_TEMPLATE.substitute(test_file='aviclient_test.go')
     LOG.info("executing command %s ", cmd)
     out = subprocess.check_output(shlex.split(cmd))
     LOG.info("playbook out %s ", out)
