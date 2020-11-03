c642488592cbafdd91ca8c6646562018099d8752
vijay-avi <vishankar@vmware.com>
2020-11-03 08:19:23 +0000

    Updating the version file with 18.2.12

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '18.2.11'
-AVI_PIP_VERSION = '18.2.11b1'
+AVI_VERSION = '18.2.12'
+AVI_PIP_VERSION = '18.2.12b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
