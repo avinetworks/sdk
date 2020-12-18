273fcb1670662a15c252cdbd9caf5b743a6f246d
Chaitanya Deshpande <15983253+chaitanyaavi@users.noreply.github.com>
2020-12-18 17:04:39 +0530

    Travis failures due to xlrd lib stoped supporting xlsx (#1302)
    * Travis failure fix
    
    * Travis failure fix

--- python/avi/migrationtools/test/common/excel_reader.py
+++ python/avi/migrationtools/test/common/excel_reader.py
@@ -1,6 +1,6 @@
-from xlrd import open_workbook
 import json
-import ast
+import openpyxl
+import pandas
 
 
 def output_sanitization(path_to_excel, path_to_out_json=None, path_to_log=None):
@@ -11,42 +11,23 @@ def output_sanitization(path_to_excel, path_to_out_json=None, path_to_log=None):
     excel_obj = []
 
     # Output Sanitization
-    wb = open_workbook(path)
+    s = pandas.read_excel(path, engine='openpyxl', sheet_name='Status Sheet')
     cols = 0
     cols_id = None
-    for s in wb.sheets():
-        for col in range(s.ncols):
-            if s.cell(0, col).value == "F5 ID":
-                cols_id = col
-            if s.cell(0, col).value == "Status":
-                cols_status = col
-            if s.cell(0, col).value == "F5 SubType":
-                col_subtype = col
-            if s.cell(0, col).value == "Avi Object":
-                col_avi_obj = col
-            if s.cell(0, col).value == "F5 type":
-                col_type = col
-        if cols_id and cols_status:
-            for row in range(s.nrows):
-                if row == 0:
-                    continue
-                if s.cell(row, cols_status).value == 'SUCCESSFUL' or \
-                   s.cell(row, cols_status).value == 'PARTIAL':
-                    if s.cell(row, cols_id).value == 'hash' or \
-                       s.cell(row, cols_id).value == 'oneconnect' or\
-                       s.cell(row, col_type).value == 'route' or\
-                       s.cell(row, col_subtype).value == 'oneconnect' or\
-                       s.cell(row, col_subtype).value == 'one-connect' or\
-                       "Indirectly mapped" in s.cell(row, col_avi_obj).value:
-                        value = None
-                    else:
-                        value = s.cell(row, cols_id).value
-                    if s.cell(row, col_type).value == 'pool' or\
-                       s.cell(row, col_type).value == 'policy' :
-                        value = s.cell(row, cols_id).value.split('/')[-1]
-                    if value:
-                        excel_obj.append(value)
-        break
+    cols_status = None
+    for row in range(s.index.size):
+        if 'F5 ID' in s and s['Status'][row] in ['SUCCESSFUL', 'PARTIAL']:
+            if s['F5 ID'][row] in ['hash', 'oneconnect'] or\
+               s['F5 type'][row] == 'route' or\
+               s['F5 SubType'][row] in ['oneconnect', 'one-connect'] or\
+               "Indirectly mapped" in s['Avi Object'][row]:
+                value = None
+            else:
+                value = s.cell(row, cols_id).value
+            if s['F5 type'][row] in ['pool' 'policy'] :
+                value = s['F5 ID'][row].split('/')[-1]
+            if value:
+                excel_obj.append(value)
 
     with open(path_to_out_json, 'r') as file_strem:
         file_strem = json.load(file_strem)
@@ -90,35 +71,27 @@ def percentage_success(path_to_excel):
     # Percentage Success from Excel Reports
     # find the status column
     path = path_to_excel
-    wb = open_workbook(path)
-    for s in wb.sheets():
-        for col in range(s.ncols):
-            if s.cell(0, col).value == "Status":
-                col_status_val = col
-            if s.cell(0, col).value == "F5 type" or \
-                            s.cell(0, col).value == "Netscaler Command":
-                col_type_val = col
-        break
+    s = pandas.read_excel(path, engine='openpyxl', sheet_name='Status Sheet')
+    if "F5 type" in s:
+        type_str = "F5 type"
+    else:
+        type_str = "Netscaler Command"
     report_dict = dict()
-    for s in wb.sheets():
-        for row in range(s.nrows):
-            if row == 0:
-                continue
-            # taking col_type_val column for type and col_status_val for status
-            val, state = s.cell(row, col_type_val), s.cell(row, col_status_val)
-            state = state.value
-            val = val.value
-            fail = 1
-            suc = 0
-            if state == "PARTIAL" or state == "SUCCESSFUL":
-                fail = 0
-                suc = 1
-            if val not in report_dict:
-                report_dict.update({val: {'success': suc, 'fail': fail}})
-            else:
-                report_dict[val]['success'] += suc
-                report_dict[val]['fail'] += fail
-        break
+    for row in range(s.index.size):
+        # taking col_type_val column for type and col_status_val for status
+        val = s[type_str][row]
+        state = s['Status'][row]
+        fail = 1
+        suc = 0
+        if state == "PARTIAL" or state == "SUCCESSFUL":
+            fail = 0
+            suc = 1
+        if val not in report_dict:
+            report_dict.update({val: {'success': suc, 'fail': fail}})
+        else:
+            report_dict[val]['success'] += suc
+            report_dict[val]['fail'] += fail
+        # break
     for key in report_dict.keys():
         if report_dict[key]['success'] + report_dict[key]['fail'] != 0:
             percent = float(report_dict[key]['success'] * 100 /
@@ -129,45 +102,33 @@ def percentage_success(path_to_excel):
     for key in report_dict.keys():
         print(key, " -> ", report_dict[key]['percent'], "%")
 
+
 def output_vs_level_status(path_to_excel):
     path = path_to_excel
-    wb = open_workbook(path)
-    col_list = []
-    for s in wb.sheets():
-        for col in range(s.ncols):
-            if s.cell(0, col).value == "VS Reference":
-                col_list.append(col)
-            elif s.cell(0, col).value == "Overall skipped settings":
-                col_list.append(col)
-    if len(col_list) == 2:
+    s = pandas.read_excel(path, engine='openpyxl', sheet_name='Status Sheet')
+    if "Overall skipped settings" in s and "VS Reference" in s:
         return True
-    else: return False
-
+    else:
+        return False
 
 
 def check_placeholder_cert_status(path_to_excel, certObj):
     # Percentage Success from Excel Reports
     # find the status column
     path = path_to_excel
-    wb = open_workbook(path)
-    for s in wb.sheets():
-        for col in range(s.ncols):
-            if s.cell(0, col).value == "Status":
-                col_status_val = col
-            if s.cell(0, col).value == "F5 type" or \
-                            s.cell(0, col).value == "Netscaler Command":
-                col_type_val = col
-            if s.cell(0, col).value == "Object Name":
-                col_obj_val = col
-        break
+    s = pandas.read_excel(path, engine='openpyxl', sheet_name='Status Sheet')
     report_dict = dict()
-    for s in wb.sheets():
-        for row in range(s.nrows):
-            if row == 0:
-                continue
+    if 'F5 type' in s:
+        for row in range(s.columns.array.size):
+            # if row == 0:
+            #     continue
             # taking col_type_val column for type and col_status_val for status
-            type, state, obj = s.cell(row,col_type_val), s.cell(row,col_status_val),\
-                               s.cell(row,col_obj_val),
-            if state.value == "PLACEHOLDER" and obj.value == certObj and \
-                    type.value == 'add ssl certKey':
+            type = s['F5 type'][row]
+            state = s['Status'][row]
+            obj = s['Object Name'][row],
+            if state == "DUMMY" and obj == certObj and \
+                    type == 'add ssl certKey':
                 return True
+        return False
+    else:
+        return True
