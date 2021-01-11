414b6e94d9e5467665860a4027112de2b726e468
Shardul Latkar <slatkar@vmware.com>
2021-01-11 11:17:58 +0000

    Added config file parameter for migration script

--- python/create_sdk_pip_packages.sh
+++ python/create_sdk_pip_packages.sh
@@ -15,14 +15,17 @@ cp avi/$PACKAGES/setup.py .
 cp avi/$PACKAGES/MANIFEST.in .
 AVI_PIP_VERSION=`python version.py`
 AVI_PIP_VERSION_TAG="$AVI_PIP_VERSION"
+AVI_CONTROLLER_VERSION=`python version.py -c`
 if [ ! -z "$2" ]; then
     AVI_PIP_VERSION_TAG="$AVI_PIP_VERSION.$2"
 fi
 sed -i s/"AVI_PIP_VERSION =.*$"/"AVI_PIP_VERSION = \'$AVI_PIP_VERSION_TAG\'"/g setup.py
 sed -i s/"__version__ =.*$"/"__version__ = \'$AVI_PIP_VERSION\'"/g avi/$PACKAGES/__init__.py
+sed -i s/"__controller_version__ =.*$"/"__controller_version__ = \'$AVI_CONTROLLER_VERSION\'"/g avi/$PACKAGES/__init__.py
 AVI_PIP_VERSION=`python version.py`
 sed -i s/"AVI_PIP_VERSION =.*$"/"AVI_PIP_VERSION = \'$AVI_PIP_VERSION\'"/g setup.py
 sed -i s/"__version__ =.*$"/"__version__ = \'$AVI_PIP_VERSION\'"/g avi/$1/__init__.py
+sed -i s/"__controller_version__ =.*$"/"__controller_version__ = \'$AVI_CONTROLLER_VERSION\'"/g avi/$1/__init__.py
 echo "creating pip package $1 version $AVI_PIP_VERSION"
 python3 setup.py sdist >> /tmp/sdk_release.txt
 if [ $1 == "sdk" ]; then
