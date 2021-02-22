4d5d603b8269863e8765a08e09120781e47b85bc
Chaitanya Deshpande <deshpandech@vmware.com>
2021-02-22 14:36:06 +0530

    #AV-107144: Add session timeouts in Java SDK (#1348)

--- java/src/com/vmware/avi/sdk/AviCredentials.java
+++ java/src/com/vmware/avi/sdk/AviCredentials.java
@@ -92,7 +92,7 @@ public class AviCredentials
     private String version;
     private String tenantUuid;
     private Integer port = 443;
-    private Integer timeout;
+    private Integer timeout = 60; // 1 min default
     private String sessionID;
     private String csrftoken;
     private String token;
