4d65b1b436e5b736f11d6b3fff57d50ac4d25121
chugh007 <rahulc@avinetworks.com>
2021-03-04 07:07:33 +0000

    Updating the version file with 18.2.13

--- python/version.py
+++ python/version.py
@@ -3,8 +3,8 @@ Created on Apr 22, 2016
 @author: grastogi
 '''
 
-AVI_VERSION = '18.2.12'
-AVI_PIP_VERSION = '18.2.12'
+AVI_VERSION = '18.2.13'
+AVI_PIP_VERSION = '18.2.13b1'
 
 if __name__ == '__main__':
     print AVI_PIP_VERSION
