3b16f5d1c50b4ef4bdb77658dcca67e21667c154
tlimbhore <tlimbhore@vmware.com>
2021-04-12 15:13:22 +0530

    Tenant added in SessionKey

--- java/src/com/vmware/avi/sdk/AviRestUtils.java
+++ java/src/com/vmware/avi/sdk/AviRestUtils.java
@@ -237,7 +237,7 @@ public class AviRestUtils {
 	}
 
 	public static String getSessionKey(AviCredentials aviCredentials) {
-		return aviCredentials.getController() + ":" + aviCredentials.getUsername() + ":" + aviCredentials.getVersion();
+		return aviCredentials.getController() + ":" + aviCredentials.getUsername() + ":" + aviCredentials.getVersion() + ":" +aviCredentials.getTenant();
 	}
 
 	/**
